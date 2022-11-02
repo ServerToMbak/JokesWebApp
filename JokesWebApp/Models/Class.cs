99907d7cf5f7684208c1fbfa6c28fd84f725874c
ServerToMbak <server78135@gmail.com>
2022-11-02 21:57:20 +0300

    Add project files.

---
+++ JokesWebApp/Models/Class.cs
@@ -0,0 +1,8 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Cıty { get; set; }
+    }
+}
---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System.Security;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public string JokeQuestions { get; set; }    
+        
+
+        public Joke()
+        {
+ 
+        }
+    }
+}
